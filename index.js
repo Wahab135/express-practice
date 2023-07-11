9061476a2666064ccee77c35d4d55c3417b93f77
Wahab <136799310+Wahab135@users.noreply.github.com>
2023-07-11 05:13:28 +0000

    Working-express

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
+var cors = require("cors");
 const router = require("./routes/productRoutes");
 const databaseController = require("./controllers/databaseController");
 const app = express();
 const port = 3005;
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/", router);
